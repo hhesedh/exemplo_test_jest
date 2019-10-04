4ad239128ea4479d8287eb689714e7a67ceee038
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-10-03 22:14:47 -0300

    finalização dos  testes com mocking

--- script2.js
+++ script2.js
@@ -4,7 +4,6 @@ const getPeoplePromise = fetch => {
   return fetch('https://swapi.co/api/people')
     .then(response => response.json())
     .then(data => {
-      console.log(data)
       return {
         count: data.count,
         results: data.results
@@ -12,10 +11,10 @@ const getPeoplePromise = fetch => {
     });
 };
 
-const getPeople = async (fetch) => {
+const getPeople = async fetch => {
   const getRequest = await fetch('https://swapi.co/api/people');
   const data = await getRequest.json();
-   return {
+  return {
     count: data.count,
     results: data.results
   };
--- script2.test.js
+++ script2.test.js
@@ -15,3 +15,24 @@ it('calls swapi to get people with a promise', () => {
     expect(data.results.length).toBeGreaterThan(5);
   });
 });
+
+// Teste com mocking
+it('getPeople returns count and results', () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 89,
+          results: [0, 1, 2, 3, 4, 5]
+        })
+    })
+  );
+
+  expect.assertions(4);
+  return swapi.getPeoplePromise(mockFetch).then(data => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith('https://swapi.co/api/people');
+    expect(data.count).toEqual(89);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
